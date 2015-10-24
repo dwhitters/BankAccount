223fa6e9e260b2c7bfe7f99c09bdec3702dcfb03
David Whitters <d.whitters@yahoo.com>
2015-10-24 09:03:10 -0400

    Testing data writing

--- src/project3/BankGUI.java
+++ src/project3/BankGUI.java
@@ -1,7 +1,18 @@
 package project3;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class BankGUI extends JFrame {
+	
+	private JPanel panel;
+	
+	public BankGUI(){
+		panel = new JPanel();
+		
+		
+		
+		
+		this.add(panel);
+	}
 
 }
