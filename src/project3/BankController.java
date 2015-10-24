7d4f1bf18c7c58ccc2468ea1e9bc3205949dc621
Allen Sietsema <sietseal@mail.gvsu.edu>
2015-10-24 17:32:33 -0400

    Moved methods from BankController to BankModel.

--- src/project3/BankController.java
+++ src/project3/BankController.java
@@ -1,36 +1,6 @@
 package project3;
 
-import java.util.GregorianCalendar;
-
 public class BankController {
 	
-	public BankController()
-	{
-		
-	}
-	
-	public void add(int number, String owner, GregorianCalendar dateOpened, double balance, double minimumBalance, double interestRate)
-	{
-		SavingsAccount addedSavingsAccount = new SavingsAccount(number, owner, dateOpened, balance, minimumBalance, interestRate);
-	}
-	
-	public void add(int number, String owner, GregorianCalendar dateOpened, double balance, double monthlyFee)
-	{
-		CheckingAccount addedSavingsAccount = new CheckingAccount(number, owner, dateOpened, balance, monthlyFee);
-	}
-	
-	public void delete()
-	{
-		
-	}
-	
-	public void update()
-	{
-		
-	}
-	
-	public void clear()
-	{
-		
-	}
+	//this class is no longer needed per Ferguson's email.
 }
--- src/project3/BankModel.java
+++ src/project3/BankModel.java
@@ -1,8 +1,36 @@
 package project3;
 
+import java.util.GregorianCalendar;
+
 import javax.swing.table.AbstractTableModel;
 
 public class BankModel extends AbstractTableModel{
+	
+	public void add(int number, String owner, GregorianCalendar dateOpened, double balance, double minimumBalance, double interestRate)
+	{
+		SavingsAccount addedSavingsAccount = new SavingsAccount(number, owner, dateOpened, balance, minimumBalance, interestRate);
+	}
+	
+	public void add(int number, String owner, GregorianCalendar dateOpened, double balance, double monthlyFee)
+	{
+		CheckingAccount addedSavingsAccount = new CheckingAccount(number, owner, dateOpened, balance, monthlyFee);
+	}
+	
+	public void delete()
+	{
+		
+	}
+	
+	public void update()
+	{
+		
+	}
+	
+	public void clear()
+	{
+		
+	}
+	
 	public Object getElementAt(int index)
 	{
 		return new Object();
