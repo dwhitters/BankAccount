1533dbefe37c0c65ec171e1f0a54b7ecca2fbe9c
David Whitters <d.whitters@yahoo.com>
2015-10-23 22:33:23 -0400

    Removed previous comments

--- src/project3/Account.java
+++ src/project3/Account.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.GregorianCalendar;
 
 public abstract class Account implements Serializable {
-	//sadkjlfhdlkjfhjlkadfjlkasnkjfdad
 	private static final long serialVersionUID = 1L;
 	private int number;
 	private String owner;
