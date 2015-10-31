2f7f74e131d000b27a4e2f70a39643c60a6e2f77
David Whitters <d.whitters@yahoo.com>
2015-10-31 13:55:29 -0400

    Revised AccountNumberSort Class

--- src/project3/AccountNumberComparator.java
+++ src/project3/AccountNumberComparator.java
@@ -5,17 +5,6 @@ import java.util.Comparator;
 public class AccountNumberComparator implements Comparator<Account>{
 	@Override
 	public int compare(Account arg0, Account arg1) {
-		int returnVal = 2;
-		
-		if(arg0.getAccountNumber() > arg1.getAccountNumber())
-			returnVal = 1;
-		
-		else if(arg0.getAccountNumber() == arg1.getAccountNumber())
-			returnVal = 0;
-		
-		else 
-			returnVal = -1;
-		
-		return returnVal;
+		return arg0.getAccountNumber() - arg1.getAccountNumber();
 	}
 }
--- src/project3/AccountOwnerComparator.java
+++ src/project3/AccountOwnerComparator.java
@@ -3,10 +3,8 @@ package project3;
 import java.util.Comparator;
 
 public class AccountOwnerComparator implements Comparator<Account>{
-
 	@Override
 	public int compare(Account o1, Account o2) {
 		return o1.getAccountOwner().compareTo(o2.getAccountOwner());
 	}
-	
 }
